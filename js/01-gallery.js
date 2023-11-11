189bd3b1a507e2820875fb6f6b279cdcc81442ae
Ivan Moshak <ivanmoshak11@gmail.com>
2023-11-11 13:23:40 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const container = document.querySelector('gallery');
+
+container.addEventListener('click', handleClick);
+
+function handleClick(event) {
+    console.log('currentTarget', event.currentTarget);
+    console.log('target', event.target);
+}
+
 console.log(galleryItems);

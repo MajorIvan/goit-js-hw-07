a317d5e1440c817ef1e9d1d6753e370f9a5a5e23
Ivan Moshak <ivanmoshak11@gmail.com>
2023-11-17 22:14:22 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,23 +5,33 @@ const container = document.querySelector('.gallery');
 
 container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
 container.style.justifyItems = 'center';
-// container.style. = '';
 
 function createMarkup(arr) {
-    return arr.map(({ preview, description }, index) => 
-        `<li data-id='${index}' class='item gallery-item'>
-            <img src='${preview}' alt='${description}' width='350'>
+    return arr.map(({ preview, original, description }, index) => 
+        `<li data-id='${index}' class="gallery_item">
+            <a class="gallery_link" href="${original}">
+                <img
+                    class="gallery_image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    style="max-width: 100%; max-height: 100%;"
+                />
+            </a>
+        </li>
     `).join('');
 };
 
 container.addEventListener('click', handleClick);
 
 function handleClick(event) {
+    event.preventDefault();
+
     if(event.target === event.currentTarget) {
         return;
     };
     
-    const currentImage = event.target.closest ('.gallery-item');
+    const currentImage = event.target.closest ('.gallery_item');
     const imageId = +currentImage.dataset.id;
 
     const instance = basicLightbox.create(`

ce877ac74ce73bc01f6e828a3586478c250e5d33
Ivan Moshak <ivanmoshak11@gmail.com>
2023-11-18 14:18:25 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -14,30 +14,131 @@ function createMarkup(arr) {
                     class="gallery_image" 
                     src="${preview}" 
                     alt="${description}" 
-                    style="max-width: 100%; max-height: 100%;"
+                    style="width: 100%; height: 100%;"
                 />
             </a>
         </li>
     `).join('');
 };
 
-container.addEventListener('click', handleClick);
+let gallery = new SimpleLightbox('.gallery_item a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.5
+});
 
-function handleClick(event) {
-    event.preventDefault();
+gallery.next()
 
-    if(event.target === event.currentTarget) {
-        return;
-    };
+// container.addEventListener('click', handleClick);
+
+// function handleClick(event) {
+//     event.preventDefault();
+
+//     if(event.target === event.currentTarget) {
+//         return;
+//     };
+
+//     const currentImage = event.target.closest ('.gallery_item');
+//     const imageId = +currentImage.dataset.id;
+
+//     let gallery = new SimpleLightbox('.gallery_item a', {
+//         captionsData: 'alt',
+//         captionDelay: 250,
+//         overlayOpacity: 0.5
+//     });
+
+//     gallery.on('show.simplelightbox', function () {
+//         `
+//             <img src='${galleryItems[imageId].original}' alt='${galleryItems[imageId].description}'>
+//         `
+//     })
+
+    // const lightbox = document.createElement('div');
+    // lightbox.id = 'lightbox';
+    // document.body.appendChild(lightbox);
+
+    // const images = document.querySelectorAll('img');
+    // images.forEach(image => {
+    //     image.addEventListener('click', event => {
+    //         lightbox.classList.add('active')
+    //         const img = document.querySelector('img')
+    //         img.src = image.src
+    //         while (lightbox.firstChild) {
+    //             lightbox.removeChild(lightbox.firstChild)
+    //         }
+    //         lightbox.appendChild(img)
+    //     });
+    // });
+
+    // lightbox.addEventListener('click',event => {
+    //     if( event.targe !== event.currentTarget) {
+    //         return;
+    //     };
+    //     lightbox.classList.remove('active')
+    // })
     
-    const currentImage = event.target.closest ('.gallery_item');
-    const imageId = +currentImage.dataset.id;
+    // const currentImage = event.target.closest ('.gallery_item');
+    // const imageId = +currentImage.dataset.id;
 
-    const instance = basicLightbox.create(`
-    <div class='modal'>
-        <img src='${galleryItems[imageId].original}' alt='${galleryItems[imageId].description}'>
-    </div>
-    `);
+    //     <div class="gallery">
+    //         <a href="images/image1.jpg"><img src="images/thumbs/thumb1.jpg" alt="" title=""/></a>
+    //         <a href="images/image2.jpg"><img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image"/></a>
+    //     </div>
 
-    instance.show();
-};
+    // const instance = basicLightbox.create(`
+    // <div class='modal'>
+    //     <img src='${galleryItems[imageId].original}' alt='${galleryItems[imageId].description}'>
+    // </div>
+    // `);
+
+    // instance.show();
+// };
+
+
+// const lightbox = document.createElement('div');
+// lightbox.id = 'lightbox';
+// document.body.appendChild(lightbox);
+
+// const images = document.querySelectorAll('img');
+// images.forEach(image => {
+//     image.addEventListener('click', event => {
+//         lightbox.classList.add('active')
+//         const img = document.querySelector('img')
+//         img.src = image.src
+//         while (lightbox.firstChild) {
+//             lightbox.removeChild(lightbox.firstChild)
+//         }
+//         lightbox.appendChild(img)
+//     });
+// });
+
+// lightbox.addEventListener('click',event => {
+//     if( event.targe !== event.currentTarget) {
+//         return;
+//     };
+//     lightbox.classList.remove('active')
+// })
+
+//     const lightbox = document.createElement('div');
+//     lightbox.id = 'lightbox';
+//     document.body.appendChild(lightbox);
+
+//     const images = document.querySelectorAll('img');
+//     images.forEach(image => {
+//         image.addEventListener('click', event => {
+//             lightbox.classList.add('active')
+//             const img = document.querySelector('img')
+//             img.src = image.src
+//             while (lightbox.firstChild) {
+//                 lightbox.removeChild(lightbox.firstChild)
+//             }
+//             lightbox.appendChild(img)
+//         });
+//     });
+
+//     lightbox.addEventListener('click',event => {
+//         if( event.targe !== event.currentTarget) {
+//             return;
+//         };
+//         lightbox.classList.remove('active')
+//     })
